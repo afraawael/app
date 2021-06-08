f6291232647e3e4514e5222ae09da6770e874ea3
afraa <afraawael96@gmail.com>
2021-06-09 00:26:13 +0300

    lab1

---
+++ src/component/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className='footer'>
+                <h3>&copy;Afraa Othman</h3>
+            </div>
+        )
+    }
+}
+export default Footer;
---
+++ src/component/hornedBeast.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+
+class HornedBeast extends React.Component {
+    render() {
+        return (
+            <div className='Honer'>
+                <h2> {this.props.titel}</h2>
+                <p>{this.props.prag}</p>
+                <img src={this.props.url} alt={this.props.alt} />
+
+            </div>
+
+        );
+    }
+}
+export default HornedBeast;
